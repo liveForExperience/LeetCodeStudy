f4e979e1f016f5be65037e4341cb981c80a0a586
ChenYue <liveforexperience@126.com>
2019-07-09 13:27:18 +0800

    提交题832反转图像1解，更新week_1笔记

---
+++ src/main/java/com/bottomlord/week_1/quiz_832/LeetCode_832_1.java
@@ -0,0 +1,25 @@
+package com.bottomlord.week_1.quiz_832;
+
+/**
+ * @author LiveForExperience
+ * @date 2019/7/9 13:10
+ */
+public class LeetCode_832_1 {
+    public int[][] flipAndInvertImage(int[][] A) {
+        for (int[] row: A) {
+            int last = row.length - 1;
+            for (int i = 0; i <= last - i; i++) {
+                if (i == last - i) {
+                    row[i] = 1 - row[i];
+                } else {
+                    row[i] = (1- row[i]) ^ (1 - row[last - i]);
+                    row[last - i] = row[i] ^ (1 - row[last - i]);
+                    row[i] = row[i] ^ row[last - i];
+                }
+
+            }
+        }
+
+        return A;
+    }
+}
