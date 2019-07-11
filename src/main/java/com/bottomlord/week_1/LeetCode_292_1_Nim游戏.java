be33092ef0b37224eed7bf734c51a9a22cef0cb4
ChenYue <liveforexperience@126.com>
2019-07-11 19:01:43 +0800

    提交题292Nim游戏1解，更新week_1笔记;

---
+++ src/main/java/com/bottomlord/week_1/LeetCode_292_1_Nim游戏.java
@@ -0,0 +1,11 @@
+package com.bottomlord.week_1;
+
+/**
+ * @author LiveForExperience
+ * @date 2019/7/11 18:38
+ */
+public class LeetCode_292_1_Nim游戏 {
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
