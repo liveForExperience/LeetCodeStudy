7ca4bf12821dcd6d7252c8b2835748a46acc108b
ChenYue <liveforexperience@126.com>
2019-07-15 13:28:48 +0800

    提交题344反转字符串2解，week_2笔记

---
+++ src/main/java/com/bottomlord/week_2/LeetCode_344_2.java
@@ -0,0 +1,16 @@
+package com.bottomlord.week_2;
+
+/**
+ * @author LiveForExperience
+ * @date 2019/7/15 13:23
+ */
+public class LeetCode_344_2 {
+    public void reverseString(char[] s) {
+        int head = 0, tail = s.length - 1;
+        while (head < tail) {
+            s[head] ^= s[tail];
+            s[tail] ^= s[head];
+            s[head++] ^= s[tail++];
+        }
+    }
+}
