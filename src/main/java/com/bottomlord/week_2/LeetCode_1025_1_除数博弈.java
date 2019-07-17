f515dfc444d7bcf281000d7fb8fe436caaade648
ChenYue <liveforexperience@126.com>
2019-07-17 13:26:58 +0800

    提交题1025除数博弈1解，更新week_2笔记，更新README文档

---
+++ src/main/java/com/bottomlord/week_2/LeetCode_1025_1_除数博弈.java
@@ -0,0 +1,11 @@
+package com.bottomlord.week_2;
+
+/**
+ * @author LiveForExperience
+ * @date 2019/7/17 13:14
+ */
+public class LeetCode_1025_1_除数博弈 {
+    public boolean divisorGame(int N) {
+        return N % 2 == 0;
+    }
+}
