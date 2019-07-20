0046b5f82cc5031aafcf6f253ef02e55268b5414
ChenYue <liveforexperience@126.com>
2019-07-20 17:11:34 +0800

    提交题258各位相加2解，更新week_2笔记，更新README文档

---
+++ src/main/java/com/bottomlord/week_2/LeetCode_258_2.java
@@ -0,0 +1,11 @@
+package com.bottomlord.week_2;
+
+/**
+ * @author LiveForExperience
+ * @date 2019/7/20 16:43
+ */
+public class LeetCode_258_2 {
+    public int addDigits(int num) {
+        return (num - 1) % 9 + 1;
+    }
+}
