54b99ffc6267b9137fec616a4e2e376170d90a49
ChenYue <liveforexperience@126.com>
2019-07-22 16:12:44 +0800

    新增题521最长特殊序列I1解，更新week_2笔记，更新README文档

---
+++ src/main/java/com/bottomlord/week_3/LeetCode_521_1_最长特殊序列I.java
@@ -0,0 +1,14 @@
+package com.bottomlord.week_3;
+
+/**
+ * @author LiveForExperience
+ * @date 2019/7/22 13:33
+ */
+public class LeetCode_521_1_最长特殊序列I {
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b)) {
+            return -1;
+        }
+        return a.length() > b.length() ? a.length() : b.length();
+    }
+}
