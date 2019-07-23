71d763114ecd692733cc8bae1a12c24bd2ac576d
ChenYue <liveforexperience@126.com>
2019-07-23 14:08:09 +0800

    提交题463岛屿的周长1解，更新week_2笔记，更新README文档

---
+++ src/main/java/com/bottomlord/week_3/LeetCode_463_1_岛屿的周长.java
@@ -0,0 +1,35 @@
+package com.bottomlord.week_3;
+
+/**
+ * @author LiveForExperience
+ * @date 2019/7/23 13:47
+ */
+public class LeetCode_463_1_岛屿的周长 {
+    public int islandPerimeter(int[][] grid) {
+        int count = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    if (i - 1 < 0 || grid[i - 1][j] == 0) {
+                        count++;
+                    }
+
+                    if (i + 1 > grid.length - 1 || grid[i + 1][j] == 0) {
+                        count++;
+                    }
+
+                    if (j - 1 < 0 || grid[i][j - 1] == 0) {
+                        count++;
+                    }
+
+                    if (j + 1 > grid[i].length - 1 || grid[i][j + 1] == 0) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
