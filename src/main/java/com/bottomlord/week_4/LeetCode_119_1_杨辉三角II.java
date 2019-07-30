790eb22bf078d673ace67af13ea78c86c450d279
ChenYue <liveforexperience@126.com>
2019-07-30 13:22:14 +0800

    提交题119杨辉三角II3解，更新week_4笔记，更新README文档

---
+++ src/main/java/com/bottomlord/week_4/LeetCode_119_1_杨辉三角II.java
@@ -0,0 +1,34 @@
+package com.bottomlord.week_4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author LiveForExperience
+ * @date 2019/7/30 12:40
+ */
+public class LeetCode_119_1_杨辉三角II {
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> list = new ArrayList<>(rowIndex);
+
+        for (int i = 0; i < rowIndex + 1; i++) {
+            List<Integer> row = new ArrayList<>(i + 1);
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                    continue;
+                }
+
+                row.add(list.get(i - 1).get(j - 1) + list.get(i - 1).get(j));
+            }
+            list.add(row);
+        }
+
+        return list.get(rowIndex);
+    }
+
+    public static void main(String[] args) {
+        LeetCode_119_1_杨辉三角II test = new LeetCode_119_1_杨辉三角II();
+        test.getRow(3);
+    }
+}
