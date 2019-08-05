a7e7c3086064c5e4fa98a4f8f00d3a6809c40162
ChenYue <liveforexperience@126.com>
2019-08-05 23:40:50 +0800

    提交题453最小移动次数使数组元素相等1解

---
+++ src/main/java/com/bottomlord/week_5/LeetCode_453_1_最小移动次数使数组元素相等.java
@@ -0,0 +1,18 @@
+package com.bottomlord.week_5;
+
+import java.util.Arrays;
+
+/**
+ * @author LiveForExperience
+ * @date 2019/8/5 22:46
+ */
+public class LeetCode_453_1_最小移动次数使数组元素相等 {
+    public int minMoves(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0;
+        for (int i = nums.length - 1; i > 0; i--) {
+            ans += nums[i] - nums[i - 1];
+        }
+        return ans;
+    }
+}
