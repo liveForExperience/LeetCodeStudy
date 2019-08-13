c0b715e35e07ec55748c0d9c0eadc5c9575dce41
ChenYue <liveforexperience@126.com>
2019-08-13 23:43:01 +0800

    提交题993二叉树的堂兄弟节点1解

---
+++ src/main/java/com/bottomlord/week_6/LeetCode_993_1_二叉树的堂兄弟节点.java
@@ -0,0 +1,27 @@
+package com.bottomlord.week_6;
+
+import com.bottomlord.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode_993_1_二叉树的堂兄弟节点 {
+    private Map<Integer, Integer> depthMap = new HashMap<>();
+    private Map<Integer, TreeNode> parMap = new HashMap<>();
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        dfs(root, null);
+        return depthMap.get(x).equals(depthMap.get(y)) && parMap.get(x) != parMap.get(y);
+    }
+
+    private void dfs(TreeNode node, TreeNode par) {
+        if (node == null) {
+            return;
+        }
+
+        depthMap.put(node.val, par != null ? depthMap.get(par.val) + 1: 0);
+        parMap.put(node.val, par);
+        dfs(node.left, node);
+        dfs(node.right, node);
+    }
+}
