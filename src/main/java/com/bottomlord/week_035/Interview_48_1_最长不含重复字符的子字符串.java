ca15c10fa2a60e5536b164bd921023ac31584818
ChenYue <liveforexperience@126.com>
2020-03-02 12:03:25 +0800

    提交面试题48最长不含重复字符的子字符串1解

---
+++ src/main/java/com/bottomlord/week_035/Interview_48_1_最长不含重复字符的子字符串.java
@@ -0,0 +1,31 @@
+package com.bottomlord.week_035;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ThinkPad
+ * @date 2020/3/2 9:05
+ */
+public class Interview_48_1_最长不含重复字符的子字符串 {
+    public int lengthOfLongestSubstring(String s) {
+        char[] cs = s.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+        int start = 0, max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(cs[i]) && map.get(cs[i]) >= start) {
+                max = Math.max(i - start, max);
+                start = map.get(cs[i]) + 1;
+            }
+            map.put(cs[i], i);
+        }
+
+        return Math.max(max, s.length() - start);
+    }
+
+    public static void main(String[] args) {
+        Interview_48_1_最长不含重复字符的子字符串 t = new Interview_48_1_最长不含重复字符的子字符串();
+        t.lengthOfLongestSubstring("abba");
+    }
+}
