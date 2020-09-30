e0fd01bee20d894ff92114ba4341d0bd4bf19aad
ChenYue <liveforexperience@126.com>
2020-09-30 15:52:57 +0800

    题294翻转游戏II1解

---
+++ src/main/java/com/bottomlord/week_065/LeetCode_294_1_翻转游戏II.java
@@ -0,0 +1,36 @@
+package com.bottomlord.week_065;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author ChenYue
+ * @date 2020/9/30 8:45
+ */
+public class LeetCode_294_1_翻转游戏II {
+    private Map<String, Boolean> map = new HashMap<>();
+    public boolean canWin(String s) {
+        if (map.containsKey(s)) {
+            return map.get(s);
+        }
+
+        char[] cs = s.toCharArray();
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i - 1) == '+' && s.charAt(i) == '+') {
+                cs[i - 1] = '-';
+                cs[i] = '-';
+                String str = new String(cs);
+                if (!canWin(str)) {
+                    return true;
+                }
+                map.put(str, true);
+                cs[i - 1] = '+';
+                cs[i] = '+';
+            }
+        }
+
+        return false;
+    }
+}
