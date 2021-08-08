a97f6406390607acb17062a49a3c54e21f2630f6
Chen Yue <liveforexperience@126.com>
2021-08-08 12:00:43 +0800

    题457换型数组是否存在循环2解

---
+++ src/main/java/com/bottomlord/week_107/LeetCode_457_1_环形数组是否存在循环.java
@@ -0,0 +1,38 @@
+package com.bottomlord.week_107;
+
+public class LeetCode_457_1_环形数组是否存在循环 {
+    public boolean circularArrayLoop(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (check(nums, i)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean check(int[] nums, int start) {
+        int cur = start, k = 0, n = nums.length;
+        boolean flag = nums[cur] > 0;
+
+        while (k < n) {
+            int next = ((cur + nums[cur]) % n + n) % n;
+            if (flag && nums[next] < 0) {
+                return false;
+            }
+
+            if (!flag && nums[next] > 0) {
+                return false;
+            }
+
+            if (next == start) {
+                return k > 0;
+            }
+
+            cur = next;
+            k++;
+        }
+
+        return false;
+    }
+}
---
+++ src/main/java/com/bottomlord/week_107/LeetCode_457_2.java
@@ -0,0 +1,48 @@
+package com.bottomlord.week_107;
+
+/**
+ * @author chen yue
+ * @date 2021-08-07 14:54:32
+ */
+public class LeetCode_457_2 {
+    public boolean circularArrayLoop(int[] nums) {
+        int n = nums.length;
+        int[] memo = new int[n + 1];
+        for (int i = 0, idx = 1; i < n; i++, idx++) {
+            if (memo[i] > 0) {
+                continue;
+            }
+
+            int k = 0, cur = i;
+            boolean flag = nums[cur] > 0;
+            while (k <= n) {
+                int next = ((cur + nums[cur]) %  n + n) % n;
+                if (cur == next) {
+                    break;
+                }
+
+                if (flag && nums[next] < 0) {
+                    break;
+                }
+
+                if (!flag && nums[next] > 0) {
+                    break;
+                }
+
+                if (memo[next] > 0) {
+                    if (memo[next] != idx) {
+                        break;
+                    }
+
+                    return true;
+                }
+
+                k++;
+                cur = next;
+                memo[next] = idx;
+            }
+        }
+
+        return false;
+    }
+}
