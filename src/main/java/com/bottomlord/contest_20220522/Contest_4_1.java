53b527cb03af64412722c8288e0aabd5dd9dd6ae
Chen Yue <liveforexperience@126.com>
2022-05-22 21:47:37 +0800

    update

--- src/main/java/com/bottomlord/contest_20220522/Contest_4_1.java
+++ src/main/java/com/bottomlord/contest_20220522/Contest_4_1.java
@@ -7,17 +7,6 @@ package com.bottomlord.contest_20220522;
 public class Contest_4_1 {
 
     public int totalStrength(int[] strength) {
-
-    }
-
-    static class Tree {
-
-    }
-
-    static class TreeNode {
-        private int start, end;
-        private TreeNode left, right;
-        private int min, sum;
-        private boolean mark;
+        return 0;
     }
 }
