c5e7a5c2f21e4f30d13838cd5265a01c12afbe70
Chen Yue <liveforexperience@126.com>
2024-03-05 10:19:08 +0800

    题225用队列实现栈1解

---
+++ src/main/java/com/bottomlord/weekj_243/LeetCode_225_1_用队列实现栈.java
@@ -0,0 +1,51 @@
+package com.bottomlord.weekj_243;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * @author chen yue
+ * @date 2024-03-05 09:19:33
+ */
+public class LeetCode_225_1_用队列实现栈 {
+    class MyStack {
+
+        private Queue<Integer> in, out;
+
+        public MyStack() {
+            this.in = new ArrayDeque<>();
+            this.out = new ArrayDeque<>();
+        }
+
+        public void push(int x) {
+            in.offer(x);
+            while (!out.isEmpty()) {
+                in.offer(out.poll());
+            }
+
+            Queue<Integer> tmp = in;
+            in = out;
+            out = tmp;
+        }
+
+        public int pop() {
+            if (out.isEmpty()) {
+                return -1;
+            }
+
+            return out.poll();
+        }
+
+        public int top() {
+            if (out.isEmpty()) {
+                return -1;
+            }
+
+            return out.peek();
+        }
+
+        public boolean empty() {
+            return out.isEmpty();
+        }
+    }
+}
